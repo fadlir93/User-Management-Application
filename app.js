eb4c97674f653949a71e19da80d994b24f031b54
Fadli Ramadhan <fadlir93@gmail.com>
2019-02-23 08:10:40 +0700

    Function Search if does'nt exist

--- app.js
+++ app.js
@@ -38,6 +38,18 @@ app.get('/', function(req, res, next) {
 app.post('/user/search', function(req, res, next){
     let id = req.body.id;
 
+    client.hgetall(id, function(err, obj) {
+        if(!obj){
+            res.render('searchusers', {
+                error: 'User does not exist'
+            });
+        } else {
+            obj.id = id;
+            res.render('details', {
+                user: obj
+            })
+        }
+    })
 })
 
 app.listen(port, function() {
